868937ebb9f93dbb15b9d48fdb64e0c37de4554b
coffeecode <35342238+0xC0FFEEC0DE@users.noreply.github.com>
2019-11-01 16:49:59 +0200

    added links

--- npm.user.js
+++ npm.user.js
@@ -3,6 +3,10 @@
 // @namespace    npm-user-js
 // @version      0.1
 // @description  Load npm packages on any website from browser console
+// @homepageURL  https://github.com/0xC0FFEEC0DE/npm.user.js
+// @supportURL   https://github.com/0xC0FFEEC0DE/npm.user.js/issues
+// @downloadURL  https://raw.githubusercontent.com/0xC0FFEEC0DE/npm.user.js/master/npm.user.js
+// @updateURL    https://raw.githubusercontent.com/0xC0FFEEC0DE/npm.user.js/master/npm.user.js
 // @author       0xC0FFEEC0DE
 // @match        *://*/*
 // @grant        none
